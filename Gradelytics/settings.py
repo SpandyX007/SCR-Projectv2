efa1e71f8203b3b59f681462e4fd86ad77fd2064
freetouse1234 <freetousebmsit@gmail.com>
2024-11-30 12:42:17 +0530

    commit

--- Gradelytics/settings.py
+++ Gradelytics/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'studentdashboard',
-    'facultydashboard'
+    'facultydashboard',
+    'adminpanel'
 ]
 
 MIDDLEWARE = [
